a5228a47344418cf89b9cdef9cf4fc3d30b631b4
ericdiasblu <ericdiasblu@gmail.com>
2024-10-15 17:17:29 -0300

    about add

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.querySelectorAll('.nav-items a, #start-button').forEach(link => {
   link.addEventListener('click', function(e) {
-      e.preventDefault();
       const href = this.getAttribute('href');
-      document.querySelector(href).scrollIntoView({
-          behavior: 'smooth'
-      });
+
+      // Verifica se o link é um ID de seção da mesma página
+      if (href.startsWith('#')) {
+          e.preventDefault();
+          document.querySelector(href).scrollIntoView({
+              behavior: 'smooth'
+          });
+      } else {
+          // Se for um arquivo HTML, permita o comportamento padrão
+          window.location.href = href;
+      }
   });
 });
