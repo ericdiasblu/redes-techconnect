e6aecc577a21b746d30636e42c0feff92f12bef2
ericdiasblu <ericdiasblu@gmail.com>
2024-10-15 16:57:07 -0300

    iniciar button

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Scroll to sections smoothly
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', function(e) {
+document.querySelectorAll('.nav-items a, #start-button').forEach(link => {
+  link.addEventListener('click', function(e) {
       e.preventDefault();
       const href = this.getAttribute('href');
       document.querySelector(href).scrollIntoView({
-        behavior: 'smooth'
+          behavior: 'smooth'
       });
-    });
   });
+});
