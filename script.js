03c2a9a9d01db332b67305bde703888e4d86f0d7
Eric Dias <47448990+ericdiasblu@users.noreply.github.com>
2024-10-17 19:51:31 +0000

    button "iniciar" update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 document.querySelectorAll('.nav-items a, #start-button').forEach(link => {
-  link.addEventListener('click', function(e) {
-      const href = this.getAttribute('href');
+    link.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
 
-      // Verifica se o link é um ID de seção da mesma página
-      if (href.startsWith('#')) {
-          e.preventDefault();
-          document.querySelector(href).scrollIntoView({
-              behavior: 'smooth'
-          });
-      } else {
-          // Se for um arquivo HTML, permita o comportamento padrão
-          window.location.href = href;
-      }
-  });
+        // Verifica se o link é um ID de seção da mesma página
+        if (href.startsWith('#')) {
+            e.preventDefault(); // Previne o comportamento padrão
+            const section = document.querySelector(href); // Seleciona a seção pelo ID
+            if (section) {
+                section.scrollIntoView({
+                    behavior: 'smooth', // Rola suavemente até a seção
+                    block: 'start' // Alinha a parte superior da seção ao topo da página
+                });
+            }
+        } else {
+            // Se for um arquivo HTML, permita o comportamento padrão
+            window.location.href = href;
+        }
+    });
 });
